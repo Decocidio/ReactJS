b713b5a4b440705a0df4bf9e17711e0fac7ab8d9
Decocidio <bin4ryhunt3r@gmail.com>
2022-07-26 01:20:57 +0700

    Test.py

---
+++ Test.py
@@ -0,0 +1,99 @@
+#  countS = 0
+#  nStar = 10
+#  while countS <= nStar:
+#      print("*", end = " ")
+#      countS += 1
+
+# count1 = 0 
+# Line = 5
+# while count1 < Line:
+#     count2 = 0
+#     Star = 10
+#     while count2 < Star:
+#         print("*", end="")
+#         count2 += 1
+#     count1 += 1
+#     print()
+
+# message = "What is your name?"
+# print(message)
+
+# ascii_of_A = ord("A")
+# print(ascii_of_A)
+
+# char_of_str = chr( 99 )
+# print(char_of_str)
+
+# str1 = "This is test"
+# str2 = "Test"
+
+# if str2 in str1:
+#     print("Yes, some str2 is in str1")
+# else:
+#     print("Oh, there is no str2 in str1")
+
+# name = "Trudy"
+
+# if name not in "Alice,Bob":
+#     print("Access Denied")
+# else:
+#     print("Access Granted")
+
+# name = "Zoom"
+# first_char = name[4]
+# print(first_char)
+
+
+# x = "Hello World"
+# # print(x[0:5])
+# # print(x[6:11])
+# print(x[2:5])
+# print(x[6:9])
+# print(x[4:7])
+
+
+# message = "abcdefghijklmnopqrstuvwxyz"
+
+# for x1 in message:
+#     if x1 not in "aceiou":
+#         print( x1 )
+#     else:
+#         print("*")
+
+message = "abcdefghijklmnopqrstuvwxyz"
+for i in message:
+        if i not in "aeiou":
+            print(i)
+        else:
+            print("*")
+x = "Hello Python"
+print(x[0:5])
+print(x[6:11])
+print(x[2:5])
+print(x[6:9])
+print(x[4:7])
+
+# str1 = ",".join(["Alice","Bob"])
+# print(str1)
+
+# i = 5
+# x = str(5)
+
+# f = 232.453543
+# y = str(f)
+
+# b = False
+# z = str(b)
+
+# print(type(x),type(y),type(z))
+
+# print("Hello\nWorld")
+
+# print("Hello\tWorld")
+
+# print("Hello \'World\'")
+
+# print("Hello \"World\"")
+
+# print("Hello \\World")
+
